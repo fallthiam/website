523ecb8e1faac43b1339e402e51e1e9870c930e9
Fall <thiam.fall.perso@gmail.com>
2018-11-21 17:55:33 -0500

    Datas on experience

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js';
+import Experience from './Experience.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header class="Header">toto</Header>
+        <Experience></Experience>
         
           <img src={logo} className="App-logo" alt="logo" />
           <p>
--- src/Experience.js
+++ src/Experience.js
@@ -1,7 +1,14 @@
-import React, {Component} from 'react;'
+import React, { Component } from 'react';
 import logo from './logo.svg';
 
-class Experience extends Compomnent{
+const data = {
+	title: "machin",
+	place: "coffe-shop",
+	location: "moon",
+	summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+};
+
+class Experience extends Component{
 	render(){
 		return (
 			<div>
@@ -10,12 +17,16 @@ class Experience extends Compomnent{
 				</div>
 				<div class="experience-desc">
 					<div class="experience-title">
+						{data.title};
 					</div>
 					<div class="experience-place">
+						{data.place};
 					</div>
 					<div class="experience-location">
+						{data.location};
 					</div>
 					<div class="experience-summary">
+						{data.summary};
 					</div>
 				</div>
 			</div>
