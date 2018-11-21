f461efd449d31d6ded1fcb853ec0bd00162ed4d9
Fall <thiam.fall.perso@gmail.com>
2018-11-21 17:34:38 -0500

    second commit
    header started

---
+++ src/Experience.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react;'
+import logo from './logo.svg';
+
+class Experience extends Compomnent{
+	render(){
+		return (
+			<div>
+				<div class="experience-picture">
+					<img src={logo} className="App-logo" alt="logo" />
+				</div>
+				<div class="experience-desc">
+					<div class="experience-title">
+					</div>
+					<div class="experience-place">
+					</div>
+					<div class="experience-location">
+					</div>
+					<div class="experience-summary">
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Experience;
